a28ba8bf77307928480f300686a811297a1610b0
Lucas David <ld@macbook-pro-de-lucas.home>
2022-04-04 16:48:42 +0200

    remove unused util

--- handlers/writers/exec.ts
+++ handlers/writers/exec.ts
@@ -2,16 +2,3 @@ import { exec } from "child_process";
 import { promisify } from "util";
 
 export const execAsync = promisify(exec);
-
-export function execPromise(command: string) {
-  return new Promise(function (resolve, reject) {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        reject(error);
-        return;
-      }
-
-      resolve(stdout.trim());
-    });
-  });
-}
