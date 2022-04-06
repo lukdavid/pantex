e63764af26ceece66201efa844f7ca771d8e34d3
Lucas David <lucasdavid@hotmail.fr>
2022-04-06 16:58:44 +0200

    add tests (#3)
    Co-authored-by: Lucas David <ld@macbook-pro-de-lucas.home>

--- src/handlers/writers/latex.ts
+++ src/handlers/writers/latex.ts
@@ -3,20 +3,29 @@ import { execAsync } from "./exec";
 import { execSync } from "child_process";
 import shortid from "shortid";
 
+const cleanup = async (fileName: string) => {
+  // cleanup : delete all build files except pdf
+  execAsync(
+    `find . -name "${fileName}*" ! -name "${fileName}.pdf" -maxdepth 1 -delete`
+  );
+};
+
 export const compileLatex = async (
   fileName: string,
   runs = 2
 ): Promise<string> => {
   const texFile = `${fileName}.tex`;
   // running pdflatex at least twice is necessary to build table of contents
-  for (let i = 1; i <= runs; i++) {
-    console.log(`Compiling ${texFile}, run ${i}/${runs}`);
-    execSync(`pdflatex ${texFile}`); // running asynchroneously doesn't throw on compile errors ...
+  try {
+    for (let i = 1; i <= runs; i++) {
+      console.log(`Compiling ${texFile}, run ${i}/${runs}`);
+      execSync(`pdflatex ${texFile}`); // running asynchroneously doesn't throw on compile errors ...
+    }
+  } catch (error) {
+    cleanup(fileName);
+    throw error;
   }
-  // cleanup : delete all build files except pdf
-  execAsync(
-    `find . -name "${fileName}*" ! -name "${fileName}.pdf" -maxdepth 1 -delete`
-  );
+  cleanup(fileName);
   return `${fileName}.pdf`;
 };
 
